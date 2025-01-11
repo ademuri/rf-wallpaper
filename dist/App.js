03a2bddb0ac9f308ceaf8a5f290c3663fa2bedad
Adam DeMuri <adam.demuri@gmail.com>
2025-01-11 12:46:19 -0700

    rf-wallpaper: re-add math unit tests

--- dist/App.js
+++ dist/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './App.css';
 import { Sketch } from './components/p5-sketch';
 import { ReactP5Wrapper } from "@p5-wrapper/react";
+import { formatNumber } from './math/math';
 function App() {
     var _a = useState(0), frequency = _a[0], setFrequency = _a[1];
     return (React.createElement(React.Fragment, null,
@@ -11,7 +12,8 @@ function App() {
         React.createElement("div", null,
             React.createElement("span", null,
                 "Frequency: ",
-                frequency))));
+                formatNumber(frequency, 3),
+                "Hz"))));
 }
 export default App;
 //# sourceMappingURL=App.js.map
